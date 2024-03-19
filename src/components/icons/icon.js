9e98ec34cdd0fb9b6a48ada2ca81a8dff1504653
Jhonni10 <jhonieripianoscue@gmail.com>
2024-03-18 22:26:30 -0500

    updated and ofurcate code

--- src/components/icons/icon.js
+++ src/components/icons/icon.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import IconAppStore from './appstore';
-import IconBookmark from './bookmark';
-import IconCodepen from './codepen';
 import IconExternal from './external';
 import IconFolder from './folder';
 import IconFork from './fork';
@@ -11,21 +8,12 @@ import IconInstagram from './instagram';
 import IconLinkedin from './linkedin';
 import IconLoader from './loader';
 import IconLogo from './logo';
-import IconPlayStore from './playstore';
-import IconStar from './star';
-import IconTwitter from './twitter';
 import IconFacebook from './facebook';
 import IconWhastapp from './whastapp';
 
 
 const Icon = ({ name }) => {
   switch (name) {
-    case 'AppStore':
-      return <IconAppStore />;
-    case 'Bookmark':
-      return <IconBookmark />;
-    case 'Codepen':
-      return <IconCodepen />;
     case 'External':
       return <IconExternal/>;
     case 'Folder':
@@ -42,12 +30,6 @@ const Icon = ({ name }) => {
       return <IconLoader />;
     case 'Logo':
       return <IconLogo />;
-    case 'PlayStore':
-      return <IconPlayStore />;
-    case 'Star':
-      return <IconStar />;
-    case 'Twitter':
-      return <IconTwitter />;
     case 'Facebook':
       return <IconFacebook />;
     case 'Whatsapp':
